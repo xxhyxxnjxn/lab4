7b066b2d61b5cc14a03e2fd58e741d0949aeebff
xxhyxxjxn <hyunjin077@naver.com>
2018-12-20 15:46:44 +0900

    no1

---
+++ no1/signalthread.c
@@ -0,0 +1,64 @@
+/*20153292 JeongHyeonjin*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <signal.h>
+
+#define NUM_THREADS 3
+pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+sigset_t sigset;
+int completed;
+
+void *signal_thread(void *arg){
+	int signal,count=0;
+
+	while(1){
+		sigwait(&sigset,&signal);
+		if(signal==SIGINT){
+			printf("Signal thread: SIGINT %d\n",++count);
+			if(count>=3){
+				pthread_mutex_lock(&mutex);
+				completed=1;
+				pthread_mutex_unlock(&mutex);
+				break;
+			}
+		}
+	}
+	return arg;
+}
+
+int main(int argc, char *argv[]){
+	pthread_t tid;
+	void *result;
+	int arg,status;
+
+	if(argc<2){
+		fprintf(stderr,"Usage: signalthread time(sec)\n");
+		exit(1);
+	}
+	arg=atoi(argv[1]);
+
+	sigemptyset(&sigset);
+	sigaddset(&sigset,SIGINT);
+	status=pthread_sigmask(SIG_BLOCK,&sigset,NULL);
+	if(status!=0){
+		fprintf(stderr,"Set signal mask");
+		exit(1);
+	}
+
+	status=pthread_create(&tid,NULL,signal_thread,NULL);
+	if(status!=0){
+		fprintf(stderr,"Create thread %d",status);
+		exit(1);
+	}
+	while(1){
+		sleep(arg);
+		pthread_mutex_lock(&mutex);
+		printf("Main thread: mutex locked\n");
+		if(completed)break;
+		pthread_mutex_unlock(&mutex);
+	}
+	pthread_mutex_unlock(&mutex);
+	pthread_exit(result);
+}
